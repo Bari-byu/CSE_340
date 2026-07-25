5008f4502d4e5c804c96c8b2f41104e69897bee5
Bari-byu <bakpee@byupathway.edu>
2026-07-25 06:42:21 +0100

    wk3

--- src/models/projects.js
+++ src/models/projects.js
@@ -7,4 +7,42 @@ const getAllProjects = async () => {
     return result.rows;
 };
 
-export { getAllProjects };
+const getUpcomingProjects = async (number_of_projects) => {
+    const query = `
+        SELECT
+            sp.project_id,
+            sp.title,
+            sp.description,
+            sp.project_date AS date,
+            sp.location,
+            sp.organization_id,
+            o.name AS organization_name
+        FROM public.service_projects sp
+        JOIN public.organizations o
+            ON sp.organization_id = o.organization_id
+        WHERE sp.project_date >= CURRENT_DATE
+        ORDER BY sp.project_date ASC
+        LIMIT $1`;
+    const result = await db.query(query, [number_of_projects]);
+    return result.rows;
+};
+
+const getProjectDetails = async (id) => {
+    const query = `
+        SELECT
+            sp.project_id,
+            sp.title,
+            sp.description,
+            sp.project_date AS date,
+            sp.location,
+            sp.organization_id,
+            o.name AS organization_name
+        FROM public.service_projects sp
+        JOIN public.organizations o
+            ON sp.organization_id = o.organization_id
+        WHERE sp.project_id = $1`;
+    const result = await db.query(query, [id]);
+    return result.rows[0];
+};
+
+export { getAllProjects, getUpcomingProjects, getProjectDetails };
